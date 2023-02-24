87e4e4dd89379736b71ba1705d009b40e165d39b
Yehuda Dani Utomo <110799267+yeyebumpp@users.noreply.github.com>
2023-02-24 15:26:09 +0700

    update

--- resources/views/admin-lte/sidebar.blade.php
+++ resources/views/admin-lte/sidebar.blade.php
@@ -1,7 +1,6 @@
- <aside class="main-sidebar sidebar-dark-primary elevation-4">
+ <aside class="main-sidebar sidebar-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">{{config('app.name')}}</span>
     </a>
 
@@ -31,7 +30,7 @@
             </a>
           </li>
           <li class="nav-header">MASTER</li>
-        
+
           <li class="nav-item">
             <a href="#" class="nav-link @yield('active-data-master')">
               <i class="nav-icon fas fa-copy"></i>
@@ -85,7 +84,7 @@
               </p>
             </a>
           </li>
-         
+
           @role('admin')
               <li class="nav-header">ADMIN</li>
 
